5cd82f98f03dfc30f63b2acf8038046fa1e07075
Josh Wolfe <joshuafwolfe@gmail.com>
2016-10-10 20:24:53 -0400

    Installed twbs exporter for org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
     ("398f0209bfd642cf7a5e3e03bdc20db2822fd6746225a4bd99ccf9b26d3059d0" "9f3181dc1fabe5d58bbbda8c48ef7ece59b01bed606cfb868dd147e8b36af97c" "06dbcfac3705aaaa79e1a3264c6fd44ef0cf86ef5ed67930e4007e63a8c1e8ee" default)))
  '(package-selected-packages
    (quote
-    (company neotree spaceline web-mode use-package tablist smex s powerline php+-mode org-bullets magit let-alist ido-ubiquitous ido-complete-space-or-hyphen haskell-mode expand-region doom-themes ace-jump-mode))))
+    (ox-twbs company neotree spaceline web-mode use-package tablist smex s powerline php+-mode org-bullets magit let-alist ido-ubiquitous ido-complete-space-or-hyphen haskell-mode expand-region doom-themes ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
